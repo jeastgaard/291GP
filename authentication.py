828c794b9704892bc327266bc8df82cd53492477
Josh Eastgaard <jeastgaa@ualberta.ca>
2022-10-24 13:10:49 -0600

    Made working sign up function. Can now add user, artists, or both. It will check if username already exist. Case-sensitive.

--- authentication.py
+++ authentication.py
@@ -1,5 +1,9 @@
 import sqlite3
+import os
 import hashlib
+
+cursor = None
+connection = None
 
 
 def hash_password(password):
@@ -8,19 +12,70 @@ def hash_password(password):
     return alg.hexdigest()
 
 
-def get_authentication(cursor, connection):
-    debug_code = input('Debug?\n0: Yes\n1: No\n')
+class authenticationInterface:
+    def __init__(self, crs, conn):
+        global cursor, connection
+        cursor, connection = crs, conn
 
-    if debug_code == '0':
-        username = input("Enter username:\n")
-        password = input("Enter password: \n")
-        password = hash_password( password )
-        access = input("Enter access: \n")
-        cursor.execute('''
-        INSERT INTO users (uid, authcode, access) values
-        (:username, :password_hashed, :access);''',{'username':username,'password_hashed':password,'access':access})
+    def create_new_user(self):
+        # First we need to load in all the existing usernames.
+        all_current_users, all_current_artists = self.load_all_names()
+        # Define our Queries to add users to database.
+        add_user_query = "INSERT INTO users VALUES (:uid, :name, :pwd)"
+        new_artist_query = "INSERT INTO artists VALUES (:uid, :name, :nat, :pwd)"
 
-    elif debug_code == '1':
+        print("users:\n", all_current_users, "\nartists:", all_current_artists)
+
+        while True:
+            username = input("Please Enter A Username: ")
+
+            if [usr for usr in all_current_users if username in usr]\
+                    or [usr for usr in all_current_artists if username in usr]:
+                print("\nSorry, that username has already been taken. Please try another one:")
+            else:
+                name = input("Username is available!\nWhat should we call you? (Name)\n")
+                break
+
+        while True:
+            new_user_table = input("\n Hi " + name + '''\nWould you like to be a:
+            1: a user
+            2: an artist
+            3: both
+            Q: Cancel\n''')
+
+            if new_user_table == '1':
+                password = hash_password( input("Please enter a password:\n ") )
+                cursor.execute(add_user_query, {'uid': username, 'name': name, 'pwd': password})
+                connection.commit()
+                # todo Add error catching here
+                print("User has been created successfully!")
+                break
+            elif new_user_table == '2':
+                nationality = input("Please Enter Your Nationality:\n")
+                password = hash_password(input("Please enter a password:\n "))
+                cursor.execute( new_artist_query, {'uid': username, 'name': name, 'nat': nationality, 'pwd': password})
+                connection.commit()
+                # todo Add error catching here
+                print("User has been created successfully!")
+                break
+            elif new_user_table == '3':
+                nationality = input("Please Enter Your Nationality:\n")
+                password = hash_password(input("Please enter a password:\n "))
+                cursor.execute(new_artist_query, {'uid': username, 'name': name, 'nat': nationality, 'pwd': password})
+                connection.commit()
+                cursor.execute(add_user_query, {'uid': username, 'name': name, 'pwd': password})
+                connection.commit()
+                # todo Add error catching here
+                print("User has been created successfully!")
+                break
+            elif new_user_table.lower() == 'q':
+                break
+            else:
+                os.system('cls')
+                print("That was an incorrect choice. Please choose again.")
+    def get_authentication(self):
+        global cursor
+
         user_name = input("\n\nPlease Enter Your Username: \n")
         user_password = input("Please Enter Your Password:\n")
         user_password = hash_password( user_password )
@@ -30,20 +85,40 @@ def get_authentication(cursor, connection):
         if len(ans) != 1:
             print("Authentication Failed")
             return False
-        elif ans[0][2]==0:
-            print("You have no access")
-        elif ans[0][2] == 1:
-            print("You are a user")
-        elif ans[0][2] == 2:
-            print("You are an artist")
-        elif ans[0][2] == 3:
-            print("You are both")
         else:
-            print("EEEEERRR")
+            pass
 
-    return True
+    def get_pwd_add_user(self, tableNames ):
+        global cursor
 
+        password = hash_password( input("Please enter a password:\n ") )
+        # Find out what tables we are adding the user too.
+    def check_access( self, cursor, user_name ):
+        # Get the users interface access after they have been authenticated.
+        cursor.execute("SELECT uid FROM users where uid = :username;", {'username': user_name})
+        user = cursor.fetchone()
+        print("User:", user)
+        cursor.execute("SELECT aid FROM artists where aid = :username", {'username':user_name})
+        artist = cursor.fetchone()
+        print("Arists:", artist)
+        if user is not None :
+            if artist is not None:
+                #We are an artist and user
+                print("We are an artist and user")
+            #We are a user
+            print("We are a user")
+        elif artist is not None:
+            # We are an artist
+            print("We are an artist")
+        else:
+            print("User authenticated but no account was found.")
 
-def check_access():
-    # Get the users interface access after they have been authenticated.
-    pass
+    def load_all_names(self):
+        global cursor
+        cursor.execute('''
+        SELECT uid FROM users;''')
+        users = cursor.fetchall()
+        cursor.execute('''
+        SELECT aid FROM artists;''')
+        artists = cursor.fetchall()
+        return users, artists
--- main.py
+++ main.py
@@ -8,25 +8,35 @@ connection = None
 
 def create_connection( path ):
     global connection, cursor
-
+    # <todo> Add some error checking here!!
     connection = sqlite3.connect( path )
     cursor = connection.cursor()
 
 def close_database():
-    # Add some error checking here!!
+    # <todo> Add some error checking here!!
     global connection
     connection.commit()
     connection.close()
 
 if __name__ == '__main__':
-    path = './auth.db'
+    db_path ='./project.db'
+    authenticated = False;
+    create_connection( db_path )
 
+    auth_interface = authentication.authenticationInterface(cursor, connection)
     print( '''Welcome to the First Group Project's Song Player!\n
     Please be aware, we don't actually play any music....''' )
-    create_connection( path )
-    authenticated = False
-    while not authenticated:
-        authenticated = authentication.get_authentication(cursor, connection)
 
+    while not authenticated:
+        login_choice = input("Login (1) or Sign Up(2)? (Q) to quite\n")
+
+        if login_choice.lower() == 'q':
+            break
+        elif login_choice == '1':
+            auth_interface.get_authentication()
+        elif login_choice == '2':
+            auth_interface.create_new_user()
+        else:
+            print("Choice Unrecognized, please try again...\n")
     close_database()
-    print( 'Success')
+    print('Success')
